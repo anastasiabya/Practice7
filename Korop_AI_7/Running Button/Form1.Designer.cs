051f07774e044d25d9ac46b96e7a2818dfa1b141
anastasiabya <46025207+anastasiabya@users.noreply.github.com>
2020-04-15 23:53:35 +0300

    Add files via upload

---
+++ Korop_AI_7/Running Button/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Running_Button
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Location = new System.Drawing.Point(208, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Push me";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 381);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(490, 420);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Running Button";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Korop_AI_7/Running Button/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Running_Button
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Поздравляем! Вы смогли нажать на кнопку!");
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+            int offset = 1;
+            int r = 10;
+            if (x >= button1.Left - r && x <= button1.Right + r && y >= button1.Top - r && y <= button1.Bottom + r)
+                Running(x, y, r, offset);
+        }
+
+        public void Running(int x, int y, int r, int offset)
+        {
+                int Width = this.Width - button1.Width;
+                int Height = this.Height - button1.Height*3;
+                if ((x >= button1.Left - r) && (x <= button1.Left) && (button1.Right < Width))
+                    button1.Left = button1.Left + offset;
+                if ((x <= button1.Right + r) && (x >= button1.Right) && (button1.Left > button1.Width/2))
+                    button1.Left = button1.Left - offset;
+                if ((y <= button1.Bottom + r) && (y >= button1.Bottom) && (button1.Top > button1.Height))
+                    button1.Top = button1.Top - offset;
+                if ((y >= button1.Top - r) && (y <= button1.Top) && (button1.Bottom < Height))
+                    button1.Top = button1.Top + offset;          
+        }
+    }
+}
